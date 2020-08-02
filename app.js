897044634fa765aeec754d8be04105ec4855c904
Jakov Glavina <cyber.shot.jakov@gmail.com>
2020-08-02 22:42:21 +0200

    try fixing cors and env variables

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ mongoose.connect(process.env.MONGODB_URI, {
   .catch((error) => console.error('cannot connect to mongodb: ', error.message));
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
--- index.js
+++ index.js
@@ -18,5 +18,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(process.env.PORT || 3001, () => {
-  console.log(`Server running on port ${config.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
