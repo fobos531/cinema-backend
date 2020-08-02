9fa0a356927752fbd06801bff6b224238b3728c7
Jakov Glavina <cyber.shot.jakov@gmail.com>
2020-08-02 23:07:27 +0200

    another cors fix try

--- app.js
+++ app.js
@@ -23,13 +23,8 @@ mongoose.connect(process.env.MONGODB_URI, {
   .catch((error) => console.error('cannot connect to mongodb: ', error.message));
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+app.options('*', cors());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
